2d8927ab38de8e420ac4b3effa2b77e23d7b34e1
cin <cin@bon>
2026-02-13 14:37:19 +0300

    add feat

--- imageFeed/SingleImage/SingleImageViewController.swift
+++ imageFeed/SingleImage/SingleImageViewController.swift
@@ -11,6 +11,10 @@ final class SingleImageViewController: UIViewController {
     
     @IBOutlet private weak var imageView: UIImageView!
     
+    @IBAction private func backButton(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         imageView.image = image
