f8aad717fa94dd0a66da9f59e0c260715b8a0c41
cin <cin@bon>
2026-02-13 13:48:49 +0300

    add feat

---
+++ imageFeed/SingleImage/SingleImageViewController.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+final class SingleImageViewController: UIViewController {
+    
+    var image: UIImage? {
+        didSet{
+            guard isViewLoaded else { return }
+            imageView.image = image
+        }
+    }
+    
+    @IBOutlet private weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imageView.image = image
+    }
+    
+}
